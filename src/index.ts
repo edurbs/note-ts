80465d176e69b08b9784893739f8c74150aa75e2
Eduardo Soares <edurbs@gmail.com>
2023-11-20 22:04:08 -0300

    add routes get post

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import express from 'express';
+import express, { Request, Response } from "express";
 
 const server = express();
+
+server.use(express.json());
+
+server.get('/', (request: Request, respose: Response) => {
+  return respose.status(200).json({message: 'DioBank API'})
+});
+
+server.post('/user', (request: Request, respose: Response) => {
+  const body = request.body;
+  return respose.status(201).json({message: 'User created'})
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"));
+
